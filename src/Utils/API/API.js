85c7936282208c1147f9e32f7d81f5166353be96
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-01 20:57:49 -0600

    Start building first fetch call

---
+++ src/Utils/API/API.js
@@ -0,0 +1,10 @@
+import {key} from './key';
+
+export const getSenate = async () => {
+  const response = await fetch('https://api.propublica.org/congress/v1/115/senate/committees/SSHR.json', {
+    method: "GET",
+    headers: {"X-API-Key": `${key}`}
+  });
+  const result = await response.json()
+  console.log(result.results)
+}
