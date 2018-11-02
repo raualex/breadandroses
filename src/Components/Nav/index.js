bbb4b1d45ae8509a4875685190bb08d02b2b75a2
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-01 19:30:21 -0600

    Build small logo for nav bar

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom'
+import star from '../../Utils/Assets/Images/back_star.png';
+import fist from '../../Utils/Assets/Images/power.svg';
 
 const Nav = () => {
 
   return (
     <div className="header-bar">
-      <button className="senate-btn">
-        <NavLink to='/senate'
-          name='senate'
-        >SENATE
-        </NavLink>
-      </button>
-      <button className="house-btn">
-        <NavLink to='/house'
-          name='house'
-        >HOUSE
-        </NavLink>
-      </button>
+      <div className="btn-container">
+        <button className="senate-btn">
+          <NavLink to='/senate'
+            name='senate'
+          >SENATE
+          </NavLink>
+        </button>
+        <button className="house-btn">
+          <NavLink to='/house'
+            name='house'
+          >HOUSE
+          </NavLink>
+        </button>
+      </div>
+      <div className="nav-star-container">
+        <img src={star} className="nav-star" alt="star" />
+        <img src={fist} className="nav-fist" alt="fist" />
+      </div>
     </div>
   )
 }
