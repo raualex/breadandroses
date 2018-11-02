9d1a8223c2d9f2de853b578f8c8cca4fe593b106
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-01 20:06:56 -0600

    Finish Nav bar styles and routes

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -14,20 +14,22 @@ const Nav = () => {
         <h1 className="nav-title">ROSES</h1>
       </div>
       <div className="btn-container">
-        <button className="senate-btn">
-          <NavLink to='/senate'
-            name='senate'
-            className='senate-link'
-          >SENATE
-          </NavLink>
-        </button>
-        <button className="house-btn">
-          <NavLink to='/house'
+        <NavLink to='/senate'
+          name='senate'
+          className='senate-link'
+        >
+          <button className="senate-btn">
+            SENATE
+          </button>
+        </NavLink>
+        <NavLink to='/house'
             name='house'
             className='house-link'
-          >HOUSE
-          </NavLink>
-        </button>
+        >
+          <button className="house-btn">
+            HOUSE
+          </button>
+        </NavLink>
       </div>
       <div className="nav-star-container">
         <img src={star} className="nav-star" alt="star" />
