9a08dc11b398a235fe0b80534398965e745aa94d
Alexander Rau <alexanderrau86@yahoo.com>
2018-11-03 20:36:32 -0600

    Write more thunk tests

--- src/Thunks/thunks.test.js
+++ src/Thunks/thunks.test.js
@@ -27,16 +27,46 @@ describe('fetchSenate thunk', () => {
     expect(API.getSenate).toHaveBeenCalled()
   });
 
-  it('should dispatch hasErrored(true) if error', () => {
-    API.getSenate = jest.fn(() => return { status: 404, message: 'error' } )
+  it('should dispatch with isLoading(false) when fetch returns data', () => {
+    API.getSenate = jest.fn(() => ([ { name: 'Bernie Sanders' } ]))
     const thunk = fetchSenate()
 
     thunk(mockDispatch)
 
-    expect
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(false))
   });
 });
 
 describe('fetchHouse thunk', () => {
+    let mockDispatch;
+
+  beforeEach(() => {
+    mockDispatch = jest.fn()
+  });
   
+  it('should dispatch with the isLoading action',() => {
+    const thunk = fetchHouse()
+
+    thunk(mockDispatch)
+
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(true))
+  });
+
+  it('should fire getHouse', () => {
+    API.getHouse = jest.fn()
+    const thunk = fetchHouse()
+
+    thunk(mockDispatch)
+
+    expect(API.getHouse).toHaveBeenCalled()
+  });
+
+  it('should dispatch with isLoading(false) when fetch returns data', () => {
+    API.getHouse = jest.fn(() => ([ { name: 'Jared Polis' } ]))
+    const thunk = fetchHouse()
+
+    thunk(mockDispatch)
+
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(false))
+  });
 });
